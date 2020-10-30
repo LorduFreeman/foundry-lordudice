9353ff2788bd54b319a48d356f26035472ede90e
LorduFreeman <72170114+LorduFreeman@users.noreply.github.com>
2020-10-30 16:53:42 +0100

    First version

---
+++ lordudice/scripts/darkdeeds.js
@@ -0,0 +1,251 @@
+
+Hooks.on('diceSoNiceReady', (dice3d) => {
+  dice3d.addSystem({ id: "LCD - Dark Deeds", name: "LCD - Dark Deeds" }, false);
+
+   dice3d.addDicePreset({
+     type: "d20",
+     labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10",
+       "11",
+       "12",
+       "13",
+       "14",
+       "15",
+       "16",
+       "17",
+       "18",
+       "19",
+       "20"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds",
+	 fontScale: 0.9
+   },"d20");
+
+   dice3d.addDicePreset({
+     type: "d2",
+	      labels: [
+       "†",
+       "☀"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds"
+	 
+   });
+
+   dice3d.addDicePreset({
+     type: "d4",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds"
+	 
+   },"d4");
+
+   dice3d.addDicePreset({
+     type: "d6",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds"
+   },"d6");
+
+   dice3d.addDicePreset({
+     type: "df",
+	      labels: [
+       "†",
+       "",
+       "☀"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds",
+	 fontScale: 1.4 
+   },"df");
+   
+   dice3d.addDicePreset({
+     type: "d8",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds",
+	 fontScale: 0.8
+   },"d8");
+   
+   dice3d.addDicePreset({
+     type: "d10",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds" 
+   },"d10");
+
+   dice3d.addDicePreset({
+     type: "d12",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10",
+       "11",
+       "12"
+     ],
+	 // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds"
+   },"d12");
+   
+   dice3d.addDicePreset({
+     type: "d100",
+	      labels: [
+       "10",
+       "20",
+       "30",
+       "40",
+       "50",
+       "60",
+       "70",
+       "80",
+       "90",
+       "00",
+     ],
+	 // // colorset:"LCD - Dark Deeds colors",
+     system: "LCD - Dark Deeds",
+	 fontScale: 0.8
+   },"d10");
+   
+   
+  dice3d.addTexture("copper", {
+    name: "Dark Copper",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/rough.png",
+	bump: "modules/lordudice/graphics/dice/rough-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Dark Deeds colors',
+        description: "Dark Tarnished Copper",
+        category: "LCD - Dark Deeds",
+        background: "#1a1a1a",
+		foreground: '#800000',
+		outline: '#330000',
+        edge: '#1a1a1a',
+		texture: 'copper',
+		material: 'metal',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "df":1.4,
+          "d6":1.3,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+	
+  dice3d.addTexture("pact", {
+    name: "Forgotten Pact",
+    composite: "soft-light",
+    source: "modules/lordudice/graphics/dice/pact.png",
+	bump: "modules/lordudice/graphics/dice/pact-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Dark Desires',
+        description: "Forgotten Pact",
+        category: "LCD - Dark Deeds",
+        background: "#c8c8c8",
+		foreground: '#4e0000',
+		outline: '#990000',
+        edge: '#350000',
+		texture: 'pact',
+		material: 'plastic',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "df":1.4,
+          "d6":1.3,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+  dice3d.addTexture("sun", {
+    name: "Worthy Metal",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/sun.png",
+	bump: "modules/lordudice/graphics/dice/sun.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Saviours Blessing',
+        description: "Saviours Blessing",
+        category: "LCD - Dark Deeds",
+        background: "#fff1bb",
+		foreground: '#ff870d',
+		outline: '#ff6b00',
+        edge: '#ffbf00',
+		texture: 'sun',
+		material: 'metal',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "df":1.4,
+          "d6":1.3,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+	
+});
