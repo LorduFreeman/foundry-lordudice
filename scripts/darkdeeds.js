bf088fc4d5d7820c1f1f148f45ae5b851b27ae75
LorduFreeman <matrixretoasted@gmx.net>
2021-07-08 01:39:15 +0200

    File structure changes, workflow/release setup

---
+++ scripts/darkdeeds.js
@@ -0,0 +1,236 @@
+
+Hooks.on('diceSoNiceReady', (dice3d) => {
+  dice3d.addSystem({ id: "LCD - Dark Deeds", name: "LCD - Dark Deeds" }, false);
+
+   dice3d.addDicePreset({
+     type: "d20",
+     labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10",
+       "11",
+       "12",
+       "13",
+       "14",
+       "15",
+       "16",
+       "17",
+       "18",
+       "19",
+       "20"
+     ],
+     system: "LCD - Dark Deeds"
+   },"d20");
+
+   dice3d.addDicePreset({
+     type: "d2",
+	      labels: [
+       "†",
+       "☀"
+     ],
+     system: "LCD - Dark Deeds"
+	 
+   });
+
+   dice3d.addDicePreset({
+     type: "d4",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4"
+     ],
+     system: "LCD - Dark Deeds"
+	 
+   },"d4");
+
+   dice3d.addDicePreset({
+     type: "d6",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6"
+     ],
+     system: "LCD - Dark Deeds"
+   },"d6");
+
+   dice3d.addDicePreset({
+     type: "df",
+	      labels: [
+       "†",
+       "",
+       "☀"
+     ],
+     system: "LCD - Dark Deeds",
+	 fontScale: 1.3
+   },"df");
+   
+   dice3d.addDicePreset({
+     type: "d8",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8"
+     ],
+     system: "LCD - Dark Deeds"
+   },"d8");
+   
+   dice3d.addDicePreset({
+     type: "d10",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10"
+     ],
+     system: "LCD - Dark Deeds" 
+   },"d10");
+
+   dice3d.addDicePreset({
+     type: "d12",
+	      labels: [
+       "†",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6",
+       "7",
+       "8",
+       "9",
+       "10",
+       "11",
+       "12"
+     ],
+     system: "LCD - Dark Deeds"
+   },"d12");
+   
+   dice3d.addDicePreset({
+     type: "d100",
+	      labels: [
+       "10",
+       "20",
+       "30",
+       "40",
+       "50",
+       "60",
+       "70",
+       "80",
+       "90",
+       "00",
+     ],
+     system: "LCD - Dark Deeds"
+   },"d10");
+   
+   
+  dice3d.addTexture("copper", {
+    name: "Dark Copper",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/rough.png",
+	bump: "modules/lordudice/graphics/dice/rough-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Dark Deeds colors',
+        description: "Dark Tarnished Copper",
+        category: "LCD - Dark Deeds",
+        background: "#1a1a1a",
+		foreground: '#800000',
+		outline: '#330000',
+        edge: '#1a1a1a',
+		texture: 'copper',
+		material: 'metal',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "d6":1.2,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+	
+  dice3d.addTexture("pact", {
+    name: "Forgotten Pact",
+    composite: "soft-light",
+    source: "modules/lordudice/graphics/dice/pact.png",
+	bump: "modules/lordudice/graphics/dice/pact-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Dark Desires',
+        description: "Forgotten Pact",
+        category: "LCD - Dark Deeds",
+        background: "#c8c8c8",
+		foreground: '#4e0000',
+		outline: '#990000',
+        edge: '#350000',
+		texture: 'pact',
+		material: 'plastic',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "d6":1.2,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+  dice3d.addTexture("sun", {
+    name: "Worthy Metal",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/sun.png",
+	bump: "modules/lordudice/graphics/dice/sun.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Saviours Blessing',
+        description: "Saviours Blessing",
+        category: "LCD - Dark Deeds",
+        background: "#fff1bb",
+		foreground: '#ff870d',
+		outline: '#ff6b00',
+        edge: '#ffbf00',
+		texture: 'sun',
+		material: 'metal',
+		fontScale: {
+          "d100":0.8,
+		  "d20": 0.9,
+          "d12":1.0,
+		  "d10": 0.9,
+		  "d8": 0.9,
+          "d6":1.2,
+          "d2":1.3
+        },
+        font:"Metamorphous"
+      },"no");
+    });
+	
+});
---
+++ scripts/forbiddenknowledge.js
@@ -0,0 +1,152 @@
+
+Hooks.on('diceSoNiceReady', (dice3d) => {
+  dice3d.addSystem({ id: "LCD - Forbidden Knowledge", name: "LCD - Forbidden Knowledge" }, false);
+   
+  dice3d.addTexture("skincraft", {
+    name: "Vile Vellum",
+    composite: "darken",
+    source: "modules/lordudice/graphics/dice/skin.png",
+	bump: "modules/lordudice/graphics/dice/skin-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Dark Words Colours',
+        description: "Dark Words",
+        category: "LCD - Forbidden Knowledge",
+        background: "#3b3c50",
+		foreground: '#949494',
+		outline: '#260000',
+        edge: '#1a1b24',
+		texture: 'skincraft',
+		material: 'metal',
+		fontScale: {
+          "d100":1.1,
+		  "d20": 1.1,
+          "d12":1.2,
+		  "d10": 1.1,
+		  "d8": 1.2,
+          "d6":1.5,
+		  "d4":1.3,
+          "d2":1.3
+        },
+        font:"Jim Nightshade"
+      },"no");
+    });
+
+  dice3d.addTexture("mindbreak", {
+    name: "Dark Ooze",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/madness.png",
+	bump: "modules/lordudice/graphics/dice/madness-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Madness colors',
+        description: "Liquid Horrors",
+        category: "LCD - Forbidden Knowledge",
+        background: "#330033",
+		foreground: '#adacd5',
+		outline: '#cc0099',
+        edge: '#4d004d',
+		texture: 'mindbreak',
+		material: 'metal',
+		font:"Metal Mania"
+      },"no");
+    });
+
+  dice3d.addTexture("vines", {
+    name: "Writhing Vines",
+    composite: "overlay",
+    source: "modules/lordudice/graphics/dice/vines.png",
+	bump: "modules/lordudice/graphics/dice/vines.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Writhing Vines colors',
+        description: "Unnatural Growth",
+        category: "LCD - Forbidden Knowledge",
+        background: "#1e3000",
+		foreground: '#c9b8ad',
+		outline: '#666600',
+        edge: '#0e1600',
+		texture: 'vines',
+		material: 'plastic',
+		font:"Eater"
+      },"no");
+    });
+
+  dice3d.addTexture("brimstone", {
+    name: "Brimstone",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/brimstone.png",
+	bump: "modules/lordudice/graphics/dice/brimstone-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Path to Ruin colors',
+        description: "Old Brimstone",
+        category: "LCD - Forbidden Knowledge",
+        background: "#ff5c26",
+		foreground: '#e1b3ac',
+		outline: '#ff8000',
+        edge: '#5a0000',
+		texture: 'brimstone',
+		material: 'metal',
+		font:"Amarante"
+      },"no");
+    });
+
+  dice3d.addTexture("pyramidwall", {
+    name: "Desert Walls",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/pyramidwall.png",
+	bump: "modules/lordudice/graphics/dice/pyramidwall-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Ancient Tombs colors',
+        description: "Ancient Walls",
+        category: "LCD - Forbidden Knowledge",
+        background: "#b07248",
+		foreground: '#2f150c',
+		outline: '#432020',
+        edge: '#453629',
+		texture: 'pyramidwall',
+		material: 'metal',
+		font:"IM Fell English",
+		fontScale: {
+          "d100":0.9,
+		  "d20": 1.1,
+          "d12":1.2,
+		  "d10": 1.1,
+		  "d8": 1.0,
+          "d6":1.5,
+		  "d4":1.3,
+          "d2":1.3
+        }
+      },"no");
+    });
+
+  dice3d.addTexture("cursed", {
+    name: "Cursed Fangs",
+    composite: "multiply",
+    source: "modules/lordudice/graphics/dice/cursed.png",
+	bump: "modules/lordudice/graphics/dice/cursed-bump.png"
+  })
+    .then(() => {
+      dice3d.addColorset({
+        name: 'LCD - Cursed Visions colors',
+        description: "Cursed Visions",
+        category: "LCD - Forbidden Knowledge",
+        background: "#300040",
+		foreground: '#8600b3',
+		outline: '#a300d9',
+        edge: '#453629',
+		texture: 'cursed',
+		material: 'metal',
+		font:"Almendra"
+      },"no");
+    });
+
+
+});
